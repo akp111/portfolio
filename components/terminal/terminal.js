ca2deec8f7292ccb47cf246eb7102d21d6ad4cca
akp111 <pradhanashish95@gmail.com>
2022-06-07 00:20:06 +0530

    feat:about

--- components/terminal/terminal.js
+++ components/terminal/terminal.js
@@ -2,7 +2,6 @@ import { Container, Flex, SimpleGrid, Box } from "@chakra-ui/react"
 import { Input } from '@chakra-ui/react'
 import { useEffect, useState } from "react"
 import GLOBAL_CONSTANTS from "../../constants/gloablConstants"
-
 export default function Terminal() {
     //To set the command input by user
     const [command, setCommand] = useState(null);
@@ -15,8 +14,9 @@ export default function Terminal() {
         setCommand("")
         setCommandResponse("")
     }, [commandResponse])
+
     const HelpResponse = () => {
-        return (
+        setCommandResponse(
             <SimpleGrid columns={1} spacing={2}>
                 {Object.keys(GLOBAL_CONSTANTS.COMMAND_LIST).map((item, i) => {
                     return (
@@ -28,11 +28,32 @@ export default function Terminal() {
         )
     }
 
+    const ClearResponse = () => {
+        setAllCommandResponse([])
+        setAllCommands([])
+        setCommand("")
+        setCommandResponse("")
+    }
+
+    const ListResponse = (inputData) => {
+        return inputData.map((item, i) => { return (<b>{item}{i == inputData.length - 1 ? "." : ","} </b>) })
+    }
+
+    const AboutResponse = () => {
+        const response =
+            (<SimpleGrid columns={1} spacing={2}>
+                <p>Heya! Ashis this side. I work as developer at Ethereum Push Notification Service (aka EPNS 🔔).</p>
+                <p>I spend most of my time exploring <span>{ListResponse(GLOBAL_CONSTANTS.ABOUT_DABBLE)}</span></p>
+                <p>The tech stack I love working on are <span>{ListResponse(GLOBAL_CONSTANTS.ABOUT_LANGUAGE)}</span></p>
+                <p>In my spare time I love to <span>{ListResponse(GLOBAL_CONSTANTS.ABOUT_HOBBIES)}</span></p>
+            </SimpleGrid>);
+
+        setCommandResponse(response);
+    }
+
     const handleEnter = (e) => {
         if (e.key === 'Enter') {
             getCommandResponse(command)
-            // setAllCommands(state => [...state, command])
-            // setAllCommandResponse(state => [...state, commandResponse])
         }
     }
 
@@ -43,18 +64,19 @@ export default function Terminal() {
     const getCommandResponse = (input) => {
         switch (input && input.toLowerCase()) {
             case GLOBAL_CONSTANTS.HELP_COMMAND:
-                setCommandResponse(<HelpResponse />)
+                HelpResponse()
                 break;
             case GLOBAL_CONSTANTS.CLEAR_COMMAND:
-                setAllCommandResponse([])
-                setAllCommands([])
-                setCommand("")
-                setCommandResponse("")
+                ClearResponse()
+                break;
+            case GLOBAL_CONSTANTS.ABOUT_COMMAND:
+                AboutResponse()
                 break;
             default:
-                console.log(GLOBAL_CONSTANTS.COMMAND_NOT_FOUND_MESSAGE)
+                setCommandResponse(GLOBAL_CONSTANTS.COMMAND_NOT_FOUND_MESSAGE)
         }
     }
+
     return (
         <Container>
             {allCommands.map(((item, i) =>
--- constants/gloablConstants.js
+++ constants/gloablConstants.js
@@ -1,16 +1,17 @@
 const GLOBAL_CONSTANTS = {
-    TERMINAL_PROMT : ">",
-    HELP_COMMAND: "help",
+    TERMINAL_PROMT: ">",
+    HELP_COMMAND: `help`,
+    ABOUT_COMMAND: `about`,
+    CLEAR_COMMAND: `clear`,
+    ABOUT_LANGUAGE: ["Javasript", "NodeJS", "Typescript", "C++", "Solidity", "AWS",],
+    ABOUT_HOBBIES: ["Read", "Upskill", "Watch Anime and Web series"],
+    ABOUT_DABBLE: ["tech", "finance", "spirituality", "startup"],
     COMMAND_LIST: {
-        about : "About Ashis",
-        socials: "Connect with Ashis",
-        cv: "Download CV of Ashis",
-        exit: "See ya",
+        about: "About Ashis",
         help: "Show Commands",
         clear: "Clear Screen"
     },
-    COMMAND_NOT_FOUND_MESSAGE:"Command doesnt' exists",
-    CLEAR_COMMAND: "clear"
+    COMMAND_NOT_FOUND_MESSAGE: "Command doesnt' exists",
 }
 
 export default GLOBAL_CONSTANTS
