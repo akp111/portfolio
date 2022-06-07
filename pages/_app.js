61461a45e9986f02e7bc2791f3509c8e93113a89
akp111 <pradhanashish95@gmail.com>
2022-06-07 16:48:16 +0530

    metadata

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Head>
-        <title>Akp</title>
+      <title>Ashis: Portfolio Website</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:title" content="Ashis: Portfolio Website" key="title" />
+        <meta property="og:description" content="Ashis: Portfolio In Terminal Style" key="title" />
+        <meta name="twitter:site" content="@akp111_eth" />
+        <meta name="twitter:creator" content="@akp111_eth" />
+        <meta name="twitter:card" content="summary_large_image" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="icon" href="/A.png" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Space+Mono&display=swap" rel="stylesheet" />
       </Head>
